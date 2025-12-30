2d4d670cfb7c7a5a20149c0e00736e258044af3d
chenyu1ove <chenchongjunpro@gmail.com>
2025-12-30 17:27:32 +0800

    feat: 新增模板语法

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
-<script setup></script>
+<script setup>
+import Template from './base/模板语法/index.vue';
+</script>
 
 <template>
-  <h1>Vite + Vue 3</h1>
+  <!-- 模板语法 -->
+  <Template />
 </template>
 
-<style scoped></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+  flex-direction: column;
+  gap: 12px;
+  align-items: center;
+  justify-content: center;
+}
+
+.box {
+  border: 1px solid #000;
+  padding: 12px;
+  border-radius: 4px;
+  width: fit-content;
+}
+</style>
---
+++ src/base/模板语法/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="box">
+    <h1>模板语法</h1>
+    <!-- 文本插值 -->
+    <div class="box">
+      <h2>文本插值</h2>
+      <div>{{ 'Hello, Vue 3!' }}</div>
+      <div>{{ 5 + 3 }}</div>
+    </div>
+    <!-- 原始html -->
+    <div class="box">
+      <h2>原始 HTML</h2>
+      <div v-html="'<strong style=color:red>这是加粗的红色文本</strong>'"></div>
+    </div>
+    <!-- 属性绑定 响应式地绑定一个 attribute -->
+    <div class="box">
+      <h2>属性绑定</h2>
+      <!-- :src="'https://vuejs.org/images/logo.png'" -->
+      <img
+        v-bind:src="'https://vuejs.org/images/logo.png'"
+        alt="Vue Logo"
+        width="100"
+      />
+      <div v-bind="objectOfAttrs">动态绑定多个值</div>
+      <form v-on:submit="onSubmit"><button type="submit">提交</button></form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper',
+  style: 'background-color:green',
+};
+const onSubmit = (event) => {
+  event.preventDefault();
+  alert('表单已提交！');
+};
+</script>
